4f54ab26e612bf24d3578572a51dd9adc3fe9c8a
zakihaya <toru.hayazaki@gmail.com>
2015-10-24 09:21:32 +0000

    fix item page

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ActiveRecord::Base
 
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+
+  has_many :haves, foreign_key: "item_id", class_name: "Have" 
+  has_many :have_users, through: :haves, source: :user
+  has_many :wants, foreign_key: "item_id", class_name: "Want"
+  has_many :want_users, through: :wants, source: :user
 end
